14201ebeb8d32dfcea409f7cfd9ac851e4bf815a
aleezasiddiqui2009 <aleezasiddiqui2009@gmail.com>
2025-11-23 15:39:27 -0800

    Remove old AlphaPuzzle folder after renaming project to BrainCircuit

--- alphapuzzle/agents/random_agent.py
+++
@@ -1,6 +0,0 @@
-import random
-
-class RandomAgent:
-    def get_action(self, state):
-        # Returns a random action (0 = no-op, 1 = move)
-        return random.choice([0, 1])
--- alphapuzzle/game/game.py
+++
@@ -1,9 +0,0 @@
-from alphapuzzle.env.env import AlphaPuzzleEnv
-
-def play():
-    env = AlphaPuzzleEnv(size=4)
-    env.reset()
-    env.render()
-
-if __name__ == "__main__":
-    play()
